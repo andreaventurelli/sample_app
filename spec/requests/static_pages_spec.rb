727cd904bda6785b8f3a90bd80ef6fbc1b1eebdb
Andrea Venturelli <andrea.venturelli@mikamai.com>
2014-06-30 10:28:25 +0200

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,37 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  subject { page }
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
+    shared_examples_for "all static pages" do
+      it { should have_selector('h1', text: heading) }
+      it { should have_title(full_title(page_title)) }
     end
 
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    describe "Home page" do
+      before { visit root_path }
+      let(:heading)    { 'Sample App' }
+      let(:page_title) { '' }
+
+      it_should_behave_like "all static pages"
+      it { should_not have_title('| Home') }
     end
 
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
+    describe "Help page" do
+      before { visit help_path }
+
+      let(:heading)    { 'Help' }
+      let(:page_title) { 'Help' }
+
+      it_should_behave_like "all static pages"
     end
-  end
-
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
-  end
-
+    
   describe "About page" do
 
     it "should have the content 'About Us'" do
@@ -47,15 +41,25 @@ describe "Static pages" do
   end
 
   describe "Contact page" do
+      before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
+      it { should have_selector('h1', text: 'Contact') }
+      it { should have_title(full_title('Contact')) }
     end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
-  end
+    
+    it "should have the right links on the layout" do
+        visit root_path
+        click_link "About"
+        expect(page).to have_title(full_title('About Us'))
+        click_link "Help"
+        expect(page).to have_title(full_title('Help'))
+        click_link "Contact"
+        expect(page).to have_title(full_title('Contact'))
+        click_link "Home"
+        click_link "Sign up now!"
+        expect(page).to have_title(full_title('Sign up'))
+        click_link "sample app"
+        #expect(page).to have_title(full_title('sample app'))
+      end
+      
 end
